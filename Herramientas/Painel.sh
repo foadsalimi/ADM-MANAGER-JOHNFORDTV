11b7bc7b4c7874bdf230151090e92e0495da26ab
johndesu090 <37970753+johndesu090@users.noreply.github.com>
2020-07-03 23:27:01 +0800

    Update Painel.sh

--- Herramientas/Painel.sh
+++ Herramientas/Painel.sh
@@ -1,68 +1,68 @@
 #!/bin/bash
 Block="/etc/bin" && [[ ! -d ${Block} ]] && exit
 Block > /dev/null 2>&1
-cowsay -f eyes "esta herramienta crea el panel SSH PLUS para administrar sus vps" | lolcat 
-figlet ..dankelthaher.. | lolcat
+cowsay -f eyes "this tool creates the SSH PLUS panel to manage your vps" | lolcat 
+figlet ..johndesu090.. | lolcat
 sleep 4
 clear
 IP=$(wget -qO- ipv4.icanhazip.com)
-echo "America/Mexico_City" > /etc/timezone
-ln -fs /usr/share/zoneinfo/America/Mexico_City /etc/localtime > /dev/null 2>&1
+echo "Asia/Manila" > /etc/timezone
+ln -fs /usr/share/zoneinfo/Asia/Manila /etc/localtime > /dev/null 2>&1
  > /dev/null 2>&1
 clear
 sudo dpkg-reconfigure --frontend noninteractive tzdata
-echo -e "\E[44;1;37m           PAINEL SSHPLUS v10           \E[0m"
+echo -e "\E[44;1;37m           PANEL SSHPLUS v10           \E[0m"
 echo ""
-echo -e "                \033[1;31mATENCION"
+echo -e "                \033[1;31mATTENTION"
 echo ""
-echo -e "\033[1;32mPARA TODO USE SIEMPRE LA MISMA CONTRASENA"
+echo -e "\033[1;32mFOR EVERYTHING ALWAYS USE THE SAME PASSWORD"
 echo ""
-echo -e "\033[1;32mSIEMPRE COMFIRME LAS PREGUNTAS CON\033[1;37m Y"
+echo -e "\033[1;32mALWAYS CONFIRM QUESTIONS WITH\033[1;37m Y"
 echo ""
-echo -e "\033[1;36mINICIANDO INSTALACION"
+echo -e "\033[1;36mSTARTING INSTALLATION"
 echo ""
-echo -e "\033[1;33mESPERE..."
+echo -e "\033[1;33mWAIT..."
 apt-get update > /dev/null 2>&1
 echo ""
-echo -e "\033[1;36mINSTALACION DE APACHE2\033[0m"
+echo -e "\033[1;36mAPACHE2 INSTALLATION\033[0m"
 echo ""
-echo -e "\033[1;33mESPERE..."
+echo -e "\033[1;33mWAIT..."
 apt-get install apache2 -y > /dev/null 2>&1
 apt-get install cron curl unzip -y > /dev/null 2>&1
 echo ""
-echo -e "\033[1;36mINSTALANDO PAQUETES NECESARIOS\033[0m"
+echo -e "\033[1;36mINSTALLING NECESSARY PACKAGES\033[0m"
 echo ""
-echo -e "\033[1;33mESPERE..."
+echo -e "\033[1;33mWAIT..."
 apt-get install php5 libapache2-mod-php5 php5-mcrypt -y > /dev/null 2>&1
 service apache2 restart 
 echo ""
-echo -e "\033[1;36mINSTALACION DE MySQL\033[0m"
+echo -e "\033[1;36mINSTALLATION OF MySQL\033[0m"
 echo ""
 sleep 1
 apt-get install mysql-server -y 
 echo ""
 clear
-echo -e "                \033[1;31mATENCION"
+echo -e "                \033[1;31mATTENTION"
 echo ""
-echo -e "\033[1;32mUSE SIEMPRE LA MISMA CONTRASENA CADA QUE LA PIDA"
-echo -e "\033[1;32mCONFIRME TODAS LAS PREGUNTAS SIEMPRE USANDO \033[1;37m Y"
+echo -e "\033[1;32mALWAYS USE THE SAME PASSWORD EACH REQUEST"
+echo -e "\033[1;32mCONFIRM ALL QUESTIONS ALWAYS USING \033[1;37m Y"
 echo ""
-echo -ne "\033[1;33mEnter, Para Continuar!\033[1;37m"; read
+echo -ne "\033[1;33mEnter, To Continue!\033[1;37m"; read
 mysql_install_db
 mysql_secure_installation
 clear
-echo -e "\033[1;36mINSTALACION DE PHPMYADMIN\033[0m"
+echo -e "\033[1;36mINSTALLATION OF PHPMYADMIN\033[0m"
 echo ""
-echo -e "\033[1;31mATENCION \033[1;33m!!!"
+echo -e "\033[1;31mATTENTION \033[1;33m!!!"
 echo ""
-echo -e "\033[1;32mSELECCIONA LA OPCION \033[1;31mAPACHE2 \033[1;32mCON LA TECLA '\033[1;33mENTER\033[1;32m'"
+echo -e "\033[1;32mSELECT THE OPTION \033[1;31mAPACHE2 \033[1;32mWITH THE KEY '\033[1;33mENTER\033[1;32m'"
 echo ""
-echo -e "\033[1;32mSELECIONE \033[1;31mYES\033[1;32m EN LA SIGUIENTE OPCION (\033[1;36mdbconfig-common\033[1;32m)"
-echo -e "PARA CONFIGURAR LA BASE DE DATOS"
+echo -e "\033[1;32mSELECT \033[1;31mYES\033[1;32m IN THE NEXT OPTION (\033[1;36mdbconfig-common\033[1;32m)"
+echo -e "TO CONFIGURE THE DATABASE"
 echo ""
-echo -e "\033[1;32mRECUERDA USAR SIEMPRE LA MISMA CONTRASENA CUANDO SEA SOLICITADA"
+echo -e "\033[1;32mREMEMBER TO ALWAYS USE THE SAME PASSWORD WHEN REQUESTED"
 echo ""
-echo -ne "\033[1;33mEnter, Para Continuar!\033[1;37m"; read
+echo -ne "\033[1;33mEnter, To Continue!\033[1;37m"; read
 apt-get install phpmyadmin -y
 php5enmod mcrypt
 service apache2 restart
@@ -72,19 +72,19 @@ apt-get install php5-curl > /dev/null 2>&1
 service apache2 restart
 clear
 echo ""
-echo -e "\033[1;31mATENCION \033[1;33m!!!"
+echo -e "\033[1;31mATTENTION \033[1;33m!!!"
 echo ""
-echo -ne "\033[1;32mINTRODUZCA LA MISMA CONTRASENA\033[1;37m: "; read senha
+echo -ne "\033[1;32mENTER THE SAME PASSWORD\033[1;37m: "; read senha
 echo -e "\033[1;32mOK\033[1;37m"
 sleep 1
 mysql -h localhost -u root -p$senha -e "CREATE DATABASE plus"
 clear
-echo -e "\033[1;36mFINALIZANDO INSTALACION\033[0m"
+echo -e "\033[1;36mFINISHING INSTALLATION\033[0m"
 echo ""
-echo -e "\033[1;33mESPERE..."
+echo -e "\033[1;33mWAIT..."
 echo ""
 cd /var/www/html
-wget https://www.dropbox.com/s/2x6w32kacpkmzvc/painel10.zip > /dev/null 2>&1
+wget https://raw.githubusercontent.com/johndesu090/ADM-MANAGER-JOHNFORDTV/master/Herramientas/panel10/painel10.zip > /dev/null 2>&1
 sleep 1
 unzip painel10.zip > /dev/null 2>&1
 rm -rf painel10.zip index.html > /dev/null 2>&1
@@ -95,14 +95,14 @@ sed -i "s;suasenha;$senha;g" /var/www/html/pages/system/pass.php > /dev/null 2>&
 fi
 sleep 1
 cd
-wget https://www.dropbox.com/s/3zfjofux5ooxrwj/plus.sql > /dev/null 2>&1
+wget https://raw.githubusercontent.com/johndesu090/ADM-MANAGER-JOHNFORDTV/master/Herramientas/panel10/plus.sql > /dev/null 2>&1
 sleep 1
 if [[ -e "$HOME/plus.sql" ]]; then
     mysql -h localhost -u root -p$senha --default_character_set utf8 plus < plus.sql
     rm /root/plus.sql
 else
     clear
-    echo -e "\033[1;31mERROR Al IMPORTAR BASE DE DATOS\033[0m"
+    echo -e "\033[1;31mERROR WHEN IMPORTING DATABASE\033[0m"
     sleep 2
     exit
 fi
@@ -119,19 +119,19 @@ chmod 777 /var/www/html/admin/pages/faturas/comprovantes
 service apache2 restart
 sleep 1
 clear
-echo -e "\033[1;32mPANEL INSTALADO CON EXITO!"
+echo -e "\033[1;32mPANEL INSTALLED SUCCESSFUL!"
 echo ""
-echo -e "\033[1;36mLINK AL AREA DE ADMINISTRADOR:\033[1;37m $IP:81/html/admin\033[0m"
-echo -e "\033[1;36mLINK AL AREA DE REVENDEDOR: \033[1;37m $IP:81/html\033[0m"
-echo -e "\033[1;36mUSUARIO\033[1;37m admin\033[0m"
-echo -e "\033[1;36mCONTRASENA\033[1;37m admin\033[0m"
+echo -e "\033[1;36mLINK TO THE ADMINISTRATOR AREA:\033[1;37m $IP:81/html/admin\033[0m"
+echo -e "\033[1;36mLINK TO THE RESELLER AREA: \033[1;37m $IP:81/html\033[0m"
+echo -e "\033[1;36mUSER\033[1;37m admin\033[0m"
+echo -e "\033[1;36mPASSWORD\033[1;37m admin\033[0m"
 echo ""
-echo -e "\033[1;31mATENCION \033[1;33m!!!"
+echo -e "\033[1;31mATTENTION \033[1;33m!!!"
 echo ""
-echo -e "\033[1;33mCuando crea un servidor es necesario instalar este comando:\033[0m"
-echo -e "\033[1;33mwget http://ssh-plus.tk/revenda/confpainel/inst > /dev/null 2>&1; bash inst\033[0m"
-echo -e "\033[1;33men la vps que se convertira en servidor\033[0m"
-echo -e "\033[1;31mNOTA: \033[1;33m!!!"
+echo -e "\033[1;33mWhen creating a server you need to install this command:\033[0m"
+echo -e "\033[1;33mwget https://raw.githubusercontent.com/johndesu090/ADM-MANAGER-JOHNFORDTV/master/Herramientas/panel10/inst > /dev/null 2>&1; bash inst\033[0m"
+echo -e "\033[1;33min the vps that will become a server\033[0m"
+echo -e "\033[1;31mNOTE: \033[1;33m!!!"
 echo ""
-echo -e "\033[1;33mCambie la contrasena al iniciar sesion en el panel\033[0m"
+echo -e "\033[1;33mChange password when logging in to panel\033[0m"
 cat /dev/null > ~/.bash_history && history -c
