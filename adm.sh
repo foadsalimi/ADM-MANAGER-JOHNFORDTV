fabf61ca6f5c46b61eb9da71f4e05c016b9aa717
johndesu090 <37970753+johndesu090@users.noreply.github.com>
2020-07-03 22:37:54 +0800

    Update and rename Dankelthaher.sh to adm.sh

---
+++ adm.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+apt-get update -y; apt-get upgrade -y; wget https://raw.githubusercontent.com/johndesu090/ADM-MANAGER-JOHNFORDTV/master/install.sh; chmod 777 install.sh* && ./install.sh*
+
