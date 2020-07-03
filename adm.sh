91fa43747eb9b57d534d6fe4799a1dfd78189a72
johndesu090 <37970753+johndesu090@users.noreply.github.com>
2020-07-04 00:56:39 +0800

    Update to adm.sh

--- adm.sh
+++ adm.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-apt-get update -y; apt-get upgrade -y; wget https://raw.githubusercontent.com/johndesu090/ADM-MANAGER-JOHNFORDTV/master/install.sh; chmod 777 install.sh* && ./install.sh*
-
+apt-get update -y; apt-get upgrade -y; wget https://raw.githubusercontent.com/johndesu090/ADM-MANAGER-JOHNFORDTV/master/install.sh; chmod 777 install.sh && ./install.sh
